96fdf5dd15412c392a002d60c5916b1150987ded
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-02 16:17:47 +0530

    commit

--- udpclient.py
+++ udpclient.py
@@ -2,9 +2,11 @@ import socket
 
 msgFromClient = input("Enter the message you want to send : ")
 bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("127.0.0.1, 20001")
+serverAddressPort = ("127.0.0.1", 20001)
+bufferSize = 1024
+#creating udp socket at client
 updClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
+#send to  server using udp socket created
 updClientSocket.sendto(bytesToSend, serverAddressPort)
 
-msgFromServer = 
+msgFromServer = updClientSocket.recvfrom(bufferSize)
